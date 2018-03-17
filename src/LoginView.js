b6b21546a5a01dcafa8b2ca7c868cedd028577a2
Fiorella <fiorellaroma@hotmail.com>
2018-03-16 22:21:27 -0700

    Agregando firebase para autenticacion de usuarios con email y contrase;a

--- src/LoginView.js
+++ src/LoginView.js
@@ -8,29 +8,121 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Text
+  Text,
 } from 'react-native';
+
+import { Container, Form, Input, Item, Button, Label } from 'native-base' // 2.3.9
 
 import {Actions} from 'react-native-router-flux';
 
+import * as firebase from 'firebase';
 
+const config = {
+    apiKey: "AIzaSyD0UuMYgGGuDaAoyj8xLkOsuCdJaKtc8L0",
+    authDomain: "platzimusic-caa84.firebaseapp.com",
+    databaseURL: "https://platzimusic-caa84.firebaseio.com",
+    projectId: "platzimusic-caa84",
+    storageBucket: "platzimusic-caa84.appspot.com",
+    messagingSenderId: "59978537604"
+  };
+  firebase.initializeApp(config);
 
 export default class LoginView extends Component {
+
+  constructor(props) {
+        super(props)
+
+        this.state = ({
+            email: '',
+            password: ''
+        })
+    }
+
+    signUpUser = (email, password) => {
+      try {
+
+          if (this.state.password.length < 6) {
+              alert("Please enter atleast 6 characters")
+              return;
+          }
+
+          firebase.auth().createUserWithEmailAndPassword(email, password)
+
+      }
+      catch (error) {
+          console.log(error.toString())
+      }
+  }
+
+  loginUser = (email, password) => {
+
+      try {
+
+          firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+              console.log(user)
+              Actions.home()
+          })
+      }
+      catch (error) {
+          console.log(error.toString())
+      }
+  }
+
   handlePress() {
+    Actions.home()
+  }
+  changeRoot(){
     Actions.home()
   }
   render() {
     return (
-      <View style={styles.container}>
+      <Container style={styles.container}>
         <Text style={styles.welcome} onPress={()=>this.handlePress()}> Bienvenidos a PlatziMusic </Text>
-      </View>
+        <Form>
+          <Item floatingLabel>
+              <Label>Email</Label>
+              <Input
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  onChangeText={(email) => this.setState({ email })}
+              />
+          </Item>
+
+          <Item floatingLabel>
+              <Label>Password</Label>
+              <Input
+                  secureTextEntry={true}
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  onChangeText={(password) => this.setState({ password })}
+              />
+          </Item>
+
+          <Button style={{ marginTop: 10 }}
+              full
+              rounded
+              success
+              onPress={() => this.loginUser(this.state.email, this.state.password)}
+          >
+              <Text style={{ color: 'black' }}> Login</Text>
+          </Button>
+
+          <Button style={{ marginTop: 10 }}
+              full
+              rounded
+              primary
+              onPress={() => this.signUpUser(this.state.email, this.state.password)}
+          >
+              <Text style={{ color: 'black' }}> Sign Up</Text>
+          </Button>
+      </Form>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: 'lightgray',
     justifyContent: 'center',
     alignItems: 'center',
